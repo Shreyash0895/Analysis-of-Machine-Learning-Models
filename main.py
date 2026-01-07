518c6ec0f94a18edea342d646e48fa9ae08676e6
Shreyash0895 <118891873+Shreyash0895@users.noreply.github.com>
2026-01-07 14:34:50 +0530

    Reorganize imports and function definitions in main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from src.energy_metrics import measure_performance
 from src.task_detection import detect_task_type
 import os
 
-"""BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_PATH = os.path.join(BASE_DIR, "data", "AQI.csv")"""
+
 
 def run_pipeline(file_path, target_column):
 
@@ -19,6 +18,7 @@ def run_pipeline(df, target_column):
 
     # continue with preprocessing, training, evaluation
     return df
+
 
 
 
